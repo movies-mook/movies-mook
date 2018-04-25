e67eb9c076aad6ed04282bef978cdb7047714ad1
Javier Valiente <javara00@gmail.com>
2018-04-25 17:38:03 +0200

    viento el popa a toda vela

--- routes/movies.js
+++ routes/movies.js
@@ -3,10 +3,8 @@ const express = require("express");
 const router = express.Router();
 const mdb = require("moviedb")(process.env.API_KEY);
 
+// Búsquedas por defecto
 router.get("/rated", (req, res, next) => {
-  // mdb.genreMovies({id: 27},(err, res) => {
-  //   console.log(res);
-  // });
   mdb.miscTopRatedMovies((err, re) => {
     let data = {
       user: req.user,
@@ -30,15 +28,62 @@ router.get("/playing", (req, res) => {
     res.render("movies/index", { data });
   });
 });
+
+// Por Género
 router.get("/horror", (req, res) => {
   mdb.genreMovies({ id: 27 }, (err, re) => {
     let data = { user: req.user, movies: re.results };
     res.render("movies/index", { data });
   });
 });
+router.get("/thriller", (req, res) => {
+  mdb.genreMovies({ id: 53 }, (err, re) => {
+    let data = { user: req.user, movies: re.results };
+    res.render("movies/index", { data });
+  });
+});
+router.get("/drama", (req, res) => {
+  mdb.genreMovies({ id: 18 }, (err, re) => {
+    let data = { user: req.user, movies: re.results };
+    res.render("movies/index", { data });
+  });
+});
+router.get("/comedy", (req, res) => {
+  mdb.genreMovies({ id: 35 }, (err, re) => {
+    let data = { user: req.user, movies: re.results };
+    res.render("movies/index", { data });
+  });
+});
+router.get("/animation", (req, res) => {
+  mdb.genreMovies({ id: 16 }, (err, re) => {
+    let data = { user: req.user, movies: re.results };
+    res.render("movies/index", { data });
+  });
+});
+router.get("/adventures", (req, res) => {
+  mdb.genreMovies({ id: 12 }, (err, re) => {
+    let data = { user: req.user, movies: re.results };
+    res.render("movies/index", { data });
+  });
+});
+router.get("/action", (req, res) => {
+  mdb.genreMovies({ id: 28 }, (err, re) => {
+    let data = { user: req.user, movies: re.results };
+    res.render("movies/index", { data });
+  });
+});
+router.get("/scifi", (req, res) => {
+  mdb.genreMovies({ id: 878 }, (err, re) => {
+    let data = { user: req.user, movies: re.results };
+    res.render("movies/index", { data });
+  });
+});
+
+// Por título
 router.post("/title", (req, res) => {
   mdb.searchMovie({ query: req.body.search }, (err, re) => {
-    console.log(re);
+    let data = { user: req.user, movies: re.results };
+    res.render("movies/index", { data });
   });
 });
 
