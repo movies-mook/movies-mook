fa9fedfa8076b520179bdca0d105c9d2d3673288
davidtexmex814 <davidtexmex814.com>
2018-04-25 11:54:14 +0200

    chope

--- models/user.js
+++ models/user.js
@@ -1,24 +1,3 @@
-// const mongoose = require('mongoose');
-// const Schema   = mongoose.Schema;
-
-// const userSchema = new Schema({
-//   namereal: String,
-//   username: String,
-//   password: String,
-//   fecha: String,
-//   pais: String,
-//   path: String,
-//   img: String
-// }, {
-//   timestamps: {
-//     createdAt: 'created_at',
-//     updatedAt: 'updated_at'
-//   }
-// });
-
-// const User = mongoose.model('User', userSchema);
-// module.exports = User;
-
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
@@ -35,7 +14,7 @@ const userSchema = new Schema({
     updatedAt: 'updated_at'
   }
 });
-
+//chulo
 const User = mongoose.model('User', userSchema);
 module.exports = User;
 
