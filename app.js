a3ad7c163fc9dc5a868a14e35d423c78f53d7e51
Javier Valiente <javara00@gmail.com>
2018-04-27 11:19:35 +0200

    Finished

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // default value for title local
-app.locals.title = "LA MADRE QUE ME PARIÓ";
+app.locals.title = "NoN";
 
 const index = require("./routes/index");
 app.use("/", index);
